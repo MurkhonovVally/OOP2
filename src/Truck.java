d16295412c0f21360ca29d48a2424314c025ff7a
Vally <murkhonovbatoha@gmail.com>
2023-05-13 14:04:36 +0800

    OOP2

--- src/Truck.java
+++ src/Truck.java
@@ -1,11 +1,8 @@
-public class Truck extends Transport {
+public class Truck extends Car {
     public Truck(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
     public void checkTrailer() {
         System.out.println("Проверяем прицеп");
-    }
-    public void checkEngine() {
-        System.out.println("Проверяем двигатель");
     }
 }
